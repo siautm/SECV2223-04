ae328b2e107a2943130b6e45734fdac560d0e5bc
Ain Najiha <148193874+ainjhaa@users.noreply.github.com>
2025-06-26 11:22:18 +0800

    Update login.php

--- college_accommodation_system/login.php
+++ college_accommodation_system/login.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 $last_username = $_COOKIE['last_username'] ?? '';
@@ -11,6 +10,19 @@ include('includes/navbar.php');
     <div class="card login">
         <h2>🔐 Login</h2>
         <form action="process_login.php" method="POST">
+            <?php
+            if(isset($_GET['message'])){
+                if($_GET['message']=="failpw"){
+                    echo "<div style='background-color:pink; padding:10px; border-radius:5px; color:darkred;'>
+                    ❌ Login failed, incorrect password!</div><br>";
+                }
+                if($_GET['message']=="failuser"){
+                    echo "<div style='background-color:pink; padding:10px; border-radius:5px; color:darkred;'>
+                    ❌ Login failed, user not found!</div><br>";
+                }
+            }
+            ?>
+
             <label for="username">Username:</label>
             <input type="text" name="username" id="username" value="<?= htmlspecialchars($last_username) ?>" required>
 
